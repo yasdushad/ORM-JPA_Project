ce63e212252fe2470bcde028a901c1b99e4340e4
YashKhandelwal <Yashdushad800@gmail.com>
2024-10-01 08:18:48 +0530

    Initial Commit

---
+++ src/main/java/com/lcwd/orm/LearnigSpringOrmApplication.java
@@ -0,0 +1,13 @@
+package com.lcwd.orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnigSpringOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnigSpringOrmApplication.class, args);
+	}
+
+}
